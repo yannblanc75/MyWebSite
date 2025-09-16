31f0587eb75aa9c9c3f74cb03bdc4a5608c334fd
yannblanc75 <yann.blanc.1@outlook.fr>
2025-09-16 11:36:12 +0200

    Test : Port binding

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: PORT || 3000,
   },
   build: {
     outDir: 'dist',
