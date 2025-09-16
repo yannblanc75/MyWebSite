c0e154047ff1b353f05d27234e0abf2661fa3111
yannblanc75 <yann.blanc.1@outlook.fr>
2025-09-16 12:09:47 +0200

    ..lol

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    port: 4003,
+    port: 4004,
   },
   build: {
     outDir: 'dist',
